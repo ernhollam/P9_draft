c775899a2cdf5c5ae43ed8dbdda15108b32c634a
e.marinasy <ernholla.marinasy@gmail.com>
2023-06-12 11:31:41 +0200

    Create patients microservice

---
+++ microservice-patients/src/test/java/com/abernathyclinic/microservicepatients/MicroservicePatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.microservicepatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
