0e23db0db5dbf868a0eb7b652f1bd72dc6e3f8a3
e.marinasy <ernholla.marinasy@gmail.com>
2023-06-13 23:58:58 +0200

    Add validation annotations for Patient

--- api-patients/src/main/java/com/abernathyclinic/apipatients/model/Patient.java
+++ api-patients/src/main/java/com/abernathyclinic/apipatients/model/Patient.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +13,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -25,11 +29,19 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "patient_id")
     private Integer id;
+    @Size(max = 50, message = "Maximum of {max} characters")
+    @NotBlank(message = "Last name is mandatory")
     private String family;
+    @Size(max = 50, message = "Maximum of {max} characters")
+    @NotBlank(message = "First name is mandatory")
     private String given;
     @Column(name = "date_of_birth")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dob;
+    @Size(max = 1, message = "Maximum of {max} character, select either Male or Female")
     private String gender;
+    @Size(max = 50, message = "Maximum of {max} characters")
     private String address;
+    @Pattern(regexp = "^[0-9]{3}-[0-9]{3}-[0-9]{4}$", message = "Phone number must be in 123-456-7890 format")
     private String phone;
 }
