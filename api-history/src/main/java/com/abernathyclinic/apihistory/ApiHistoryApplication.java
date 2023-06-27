321c230e46d82985f38cb5e4b4da878aaa9004a6
e.marinasy <ernholla.marinasy@gmail.com>
2023-06-27 11:32:03 +0200

    Initial commit for notes API

---
+++ api-history/src/main/java/com/abernathyclinic/apihistory/ApiHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.apihistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHistoryApplication.class, args);
+	}
+
+}
---
+++ api-history/src/test/java/com/abernathyclinic/apihistory/ApiHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.apihistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
